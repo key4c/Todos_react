19f894706ef9399702e0f6066a19561c1ebe3c81
CitizenKey <key4citizen@gmail.com>
2025-02-21 15:14:38 +0300

    в rout.js  добавлен загрузчик и воздействие

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,19 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Form,
   Route,
 } from "react-router-dom";
 import App from "./App";
 import TodoList from "./TodoList";
 import TodoAdd from "./TodoAdd";
-// import todos from "./todos";
+import { getTodos, addTodo } from "./api.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index={true} element={<TodoList />} />
-      <Route path="add" element={<TodoAdd />} />
+      <Route index={true} element={<TodoList />} loader={getTodos} />
+      <Route path="add" element={<TodoAdd />} action={addTodo} />
     </Route>
   )
 );
